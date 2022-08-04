9135ac24df7f07bf0944325c976f7f1a8337c7d5
Tuetuopay <tuetuopay@me.com>
2022-08-04 02:45:59 +0200

    Allow for much longer write buffers

--- rat-rs-ebpf/src/main.rs
+++ rat-rs-ebpf/src/main.rs
@@ -29,17 +29,28 @@ unsafe fn try_rat_rs(ctx: ProbeContext) -> Result<u32, u32> {
     }
 
     let fd: u32 = ctx.arg(0).ok_or(0u32)?;
-    let buffer: *const u8 = ctx.arg(1).ok_or(0u32)?;
-    let len: usize = ctx.arg(2).ok_or(0u32)?;
+    let mut buffer: usize = ctx.arg(1).ok_or(0u32)?;
+    let mut len: usize = ctx.arg(2).ok_or(0u32)?;
+    let buffer_end = buffer + len;
 
-    trace!(&ctx, "write({}, 0x{}, {})", fd, buffer as usize, len);
+    trace!(&ctx, "write({}, 0x{}, {})", fd, buffer, len);
 
-    let len = len.min(BUF_SIZE);
     let mut buf = [0u8; BUF_SIZE];
-    let buf_slice = &mut buf[..len];
-    bpf_probe_read_user_buf(buffer, buf_slice).map_err(|e| e as u32)?;
 
-    WRITES.output(&ctx, buf_slice, 0);
+    // Maximum of what passes the verifier. This still nets us 256*15 = 3840 bytes, which is
+    // already a pretty long line.
+    // while loop is not possible as the ebpf verifier will not be able to prove the loop to be
+    // bounded.
+    for _ in 0..15 {
+        if buffer >= buffer_end { break }
+
+        let buf_slice = &mut buf[..len.min(BUF_SIZE)];
+        bpf_probe_read_user_buf(buffer as *const u8, buf_slice).map_err(|e| e as u32)?;
+        WRITES.output(&ctx, buf_slice, 0);
+
+        buffer += BUF_SIZE;
+        len -= BUF_SIZE;
+    }
 
     Ok(0)
 }
